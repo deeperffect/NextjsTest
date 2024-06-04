8f6f0554c88e20e9b3e6f2589a23ea2afaea8d40
Dimitar Georgiev <deeperffect@gmail.com>
2024-06-05 01:10:16 +0300

    add card component

--- src/collections/Card/Card.jsx
+++ src/collections/Card/Card.jsx
@@ -1,1 +1,27 @@
 // The Card to be exported goes here
+
+import Image from "next/image";
+import { CardColumns, CardContent, CardIcon, CardWrapper } from "./elements";
+
+const Card = ({ image, title, description }) => {
+	return (
+		<CardWrapper>
+			<CardColumns>
+				<CardIcon>
+					<Image
+						src={image.src}
+						alt={image.alt}
+						width={image.width}
+						height={image.height}
+					/>
+				</CardIcon>
+				<CardContent>
+					<h4>{title}</h4>
+					<p>{description}</p>
+				</CardContent>
+			</CardColumns>
+		</CardWrapper>
+	);
+};
+
+export default Card;
--- src/collections/Card/elements.jsx
+++ src/collections/Card/elements.jsx
@@ -1,1 +1,12 @@
 // Styled elements for the Card go here
+import styled from "styled-components";
+
+export const CardContent = styled.div``;
+
+export const CardIcon = styled.figure``;
+
+export const CardColumns = styled.div``;
+
+export const CardWrapper = styled.div`
+	background-color: ${({ theme, bgColor }) => theme[bgColor]};
+`;
